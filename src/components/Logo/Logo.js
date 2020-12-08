a901def76bab5e7ac9544c1fb0ba0a4c307b53bd
krzysztofwolny <wolny1337@gmail.com>
2020-12-08 23:17:43 +0100

    improved menu. SwitchLanguage component works on IE and Chrome

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import myphoto from '../../assets/img/me.jpg';
+import displayLanguage from '../../translations/translations';
 import './Logo.scss';
 
 const Logo = () => {
+    const text = displayLanguage();
     return(
         <div className="logo">
             <img src={myphoto} className="logo__photo"></img>
             <p className="logo__name">Krzysztof Wolny</p>
+            <p>{text.test}</p>
         </div>
     );
 }
